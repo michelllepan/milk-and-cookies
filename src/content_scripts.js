4765aef3518c38b6ae1ae6294b911455a35e6ab6
Janvi Shah <janvishah@Janvis-MacBook-Air.local>
2020-07-25 16:29:42 -0700

    content scripts

---
+++ src/content_scripts.js
@@ -0,0 +1,10 @@
+import getIngred from 'src/ingredextract';
+import getReplacer from 'src/ingredextract';
+
+/*
+var checklists = document.querySelectorAll('ul[class^="checklist dropdownwrapper list-ingredients-"]')
+
+for every ingredient in replacer
+	if checklist includes ingredient
+		replace(ingredient, thing in replacer)
+*/
--- src/ingredextract.js
+++ src/ingredextract.js
@@ -1,8 +1,8 @@
 //list of ingredient titles formatted <amount> <measurement> <name>
 var ingred_title = []
 
-//gets ingredients
-export function getIngred(){
+//get ingredients
+function getIngred(){
     //obtain both lists
     var checklists = document.querySelectorAll('ul[class^="checklist dropdownwrapper list-ingredients-"]')
     //get all the ingredients of class checkList__line
@@ -19,14 +19,12 @@ export function getIngred(){
         }
     }
     ingred_title.splice(ingred_title.length-1, ingred_title.length)
-    return ingred_title
 }
 
 //format: <amount> <measurement> <name>
-var ingredients = {}//{"all purpose flour": ["1", "cups"]}
+var ingredients = {"all purpose flour": ["1", "cups"]}
 
-//creates a dictionary -- example: "flour": [2, "cups"]
-export function addIngred(){
+function addIngred(){
     for (var i = 0; i < ingred_title.length; i++){
         var title = ingred_title[i]
         if (getMeas(title).includes("egg")) {
@@ -35,7 +33,6 @@ export function addIngred(){
             ingredients[getItem(title)] = [getVal(title), getMeas(title)]
         }
     }
-
 }
 
 //get the number value at the beginning of the ingredient title
@@ -67,7 +64,7 @@ var database = require('./database.json')
 var replacers = {}
 
 //retrieves default replacer from database for each ingredient and puts into replacers
-export function getReplacer(){
+function getReplacer(){
     for (var p in ingredients) {
         var i = 0
         while (i<database.length){
@@ -80,9 +77,11 @@ export function getReplacer(){
     }
     return replacers
 }
+getReplacer()
 
-//returns a dictionary with the original ingredient amount with the necessary replacement amount for that ingredient and notes 
-export function calculateAmount(){
+
+//returns a dictionary with the original ingredient and amount with the necessary replacement amount for that ingredient and notes 
+function calculateAmount(){
     for (var replacer in replacers){
         var conv_factor = replacers[replacer][0]/getVal(replacers[replacer][2]["replaceemeasurement"])
         console.log("Conversion Factor" + conv_factor)
@@ -96,8 +95,7 @@ export function calculateAmount(){
     return replacers
 
 }
-
-
+calculateAmount()
 
 
 
