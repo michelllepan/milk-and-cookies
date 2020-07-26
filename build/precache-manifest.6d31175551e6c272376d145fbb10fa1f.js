7219fd18736489448c9ce68d3578a3f0e589c8dc
Michelle Pan <michellepan8@gmail.com>
2020-07-25 17:58:00 -0700

    WIP

---
+++ build/precache-manifest.6d31175551e6c272376d145fbb10fa1f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f8fe2766712cb89c64343a167c1378",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66ea5dfbb21181d39cdd",
+    "url": "/static/css/main.991e940f.chunk.css"
+  },
+  {
+    "revision": "01c475a5d5f6ad440427",
+    "url": "/static/js/2.e5ffeb6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5ffeb6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ea5dfbb21181d39cdd",
+    "url": "/static/js/main.69e4637e.chunk.js"
+  },
+  {
+    "revision": "0a6efa55ba92a5e4080d",
+    "url": "/static/js/runtime-main.39a232be.js"
+  },
+  {
+    "revision": "26b43e05519181abd33eeb2e666e1aba",
+    "url": "/static/media/checkbox-selected.26b43e05.svg"
+  },
+  {
+    "revision": "6ef8d4facc36b50ab8091eac432d4788",
+    "url": "/static/media/checkbox.6ef8d4fa.svg"
+  },
+  {
+    "revision": "7177fe82b8b9de1e9b77eb61efd6c62a",
+    "url": "/static/media/down-arrow.7177fe82.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "674f1b449a20086221e76ea14bbfd7d4",
+    "url": "/static/media/up-arrow.674f1b44.svg"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var a = document.createElement("div");
+a.id = "root";
+a.style.width = "100%";
+a.style.height = "100%";
+a.style.position = "absolute"
+a.style.top = "0px"
+a.style.left = "0px"
+a.style.zIndex = 9999999;
+// document.body.appendChild(a);
+document.body.insertBefore(a, document.body.firstChild);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+console.log("HELLOOO")
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
