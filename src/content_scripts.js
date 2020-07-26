e1c1ab16fc03cfd912a92af978d7662e6e7f9848
Janvi Shah <janvishah@Janvis-MacBook-Air.local>
2020-07-25 17:51:10 -0700

    new names and fixed replacements

--- src/content_scripts.js
+++ src/content_scripts.js
@@ -1,15 +1,15 @@
 import getIngred from 'src/ingredextract';
 import getReplacer from 'src/ingredextract';
-
+import getItem from 'src/ingredextract';
 
 var names = getIngred()
 var replacements = getReplacer()
 var checklists = document.querySelectorAll('ul[class^="checklist dropdownwrapper list-ingredients-"]')
 for (var i = 0; i < checklists.length; i++){
-     var thing1 = checklists[i].getElementsByClassName("checkList__line")
-     for (var j = 0; j<thing1.length; j++){
-     	var thing2 = thing1[j].getElementsByClassName("recipe-ingred_txt added")
-     	thing2[0].innerText = replacements[x.title][2]
+     var inner1 = checklists[i].getElementsByClassName("checkList__line")
+     for (var j = 0; j<inner1.length; j++){
+     	var inner2 = inner1[j].getElementsByClassName("recipe-ingred_txt added")
+     	inner2[0].innerText = replacements[getItem(inner2.innerText)][2]
      }
 }
 
